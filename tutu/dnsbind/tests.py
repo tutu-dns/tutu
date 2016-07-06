519201e1cf89e7eb37b04788edf98682a5129ee5
Richard Franks <git@rf152.co.uk>
2016-07-06 13:34:13 +0100

    Additional tests

--- tutu/dnsbind/tests.py
+++ tutu/dnsbind/tests.py
@@ -2,6 +2,9 @@ import unittest
 
 from pyramid import testing
 
+
+from tutu.dnsbind.views.viewrecords import ViewRecords
+
 class TutuViewTests(unittest.TestCase):
 	def setUp(self):
 		self.config = testing.setUp();
@@ -10,15 +13,30 @@ class TutuViewTests(unittest.TestCase):
 		testing.tearDown();
 	
 	def test_zonelist(self):
-		from tutu.auth import Auth;
-		
-		request = testing.DummyRequest();
-    
-	def test_userlogout(self):
-		from tutu.auth import Auth;
+		from tutu.dnsbind.views.viewzones import ViewZones
 		
 		self.config.testing_securitypolicy(userid='admin', permissive=True);
-
+		
+		request = testing.DummyRequest();
+		zone = ViewZones(request);
+		response = zone.list();
+		self.assertIsInstance(response['zones'], list);
+		
+		expected = {'name': 'example.com', 'records': 6}
+		self.assertIn(expected, response['zones']);
+	
+	def test_zoneshow(self):
+		from tutu.dnsbind.views.viewzones import ViewZones
+		
+		self.config.testing_securitypolicy(userid='admin', permissive=True);
+		
+		request = testing.DummyRequest(matchdict={'zone':'example.com'});
+		
+		zone = ViewZones(request);
+		response = zone.show();
+		self.assertIsInstance(response['records'], list);
+		self.assertEquals(len(response['records']), 6);
+		
 class TutuFunctionalTests(unittest.TestCase):
 	def setUp(self):
 		from tutu import main
@@ -26,13 +44,12 @@ class TutuFunctionalTests(unittest.TestCase):
 		from webtest import TestApp
 		
 		self.testapp = TestApp(app);
+		self.testapp.post('/login', params={'username': 'admin', 'password': 'changeme'})
 	
-	def test_userlogin(self):
-		res = self.testapp.get('/login', status=200);
-		self.assertIn(b'Please enter your details', res.body);
-	
-	def test_userlogout(self):
-		res = self.testapp.get('/logout', status=302);
+	def test_zonelist(self):
+		res = self.testapp.get('/zones', status=200);
 		
+	def test_zoneshow(self):
+		res = self.testapp.get('/zone/example.com', status=200);
 
 # vim: set ts=2:
